5b4a7d770b88551fc54966243bf1b46a610fa44d
mvampa91 <67586944+mvampa91@users.noreply.github.com>
2022-03-03 22:31:46 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,19 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueMq from "vue3-mq";
 import "./assets/global.css";
 
 const app = createApp(App);
 
 app.use(router);
-
-app.use(VueMq, {
-  breakpoints: {
-    sm: 450,
-    md: 1250,
-    lg: Infinity,
-  },
-});
 
 app.mount("#app");
